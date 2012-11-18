8e0ccb2a61876a658018b2c90e1339e69e5aafef
Bernard Banta <banta.bernard@gmail.com>
2012-11-18 21:39:26 +0300

    Revert "Created health facility scaffold"
    This reverts commit 42659dcf77eb215e27763471b8ae9dc805eb4387.

--- app/models/health_facility.rb
+++
@@ -1,3 +0,0 @@
-class HealthFacility < ActiveRecord::Base
-  attr_accessible :agency, :district, :division, :hmis, :latitude, :location, :longitude, :name, :province, :srm, :sub_location, :type
-end
