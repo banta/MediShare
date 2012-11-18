64952d4368fc97ec651022a1dca2abba9033cf03
Bernard Banta <banta.bernard@gmail.com>
2012-11-18 18:02:47 +0300

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Medishare::Application.config.secret_token = 'dcfb6381f481ac6b4260ec12f7b28e9d5087e6b6a17d157bc408f61a9b65fbcbc2265d3c3e8778ad92b71048f9ebfa968208a2f83a4ff86c9667cdd51bb44437'
