e6304b60ea0b10c1e680e8e857964f34fbc098e1
Bernard Banta <banta.bernard@gmail.com>
2012-11-20 06:23:22 +0300

    Modified bednets_and_illinesses index method for datatables
    Modified bednets_and_illinesses index method for datatables

--- app/controllers/bednets_and_illinesses_controller.rb
+++ app/controllers/bednets_and_illinesses_controller.rb
@@ -6,7 +6,7 @@ class BednetsAndIllinessesController < ApplicationController
 
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: @bednets_and_illinesses }
+      format.json { render json: BednetsAndIlliness.new(view_context) }
     end
   end
 
