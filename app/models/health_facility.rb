0a35fc838318b7d5cb6a9d8df33104d4ef3b557d
Bernard Banta <banta.bernard@gmail.com>
2013-02-14 11:24:12 +0300

    Add user_id to attr_acc in health_facility model

--- app/models/health_facility.rb
+++ app/models/health_facility.rb
@@ -1,5 +1,5 @@
 class HealthFacility < ActiveRecord::Base
   belongs_to :user
-  attr_accessible :agency, :district, :division, :facility_type, :hmis, :latitude, :location, :longitude, :name, :province, :srm,
-  :sub_location
+  attr_accessible :agency, :district, :division, :facility_type, :hmis, :latitude,
+  :location, :longitude, :name, :province, :srm, :sub_location, :user_id
 end
