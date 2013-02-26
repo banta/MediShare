c0d53e9bbaf1d8a95b8a5e0d63ad43c86b393243
Bernard Banta <banta.bernard@gmail.com>
2013-02-26 10:48:22 +0300

    Did some modifications

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,7 +3,7 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-    if user.has_role? :admin?
+    if user.has_role? :admin
       can :manage, :all
     elsif user.has_role? :user
         can [:show, :update, :destroy], HealthFacility, :user_id => user.id
