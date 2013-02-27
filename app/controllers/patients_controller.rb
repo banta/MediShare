5fa9f5ab58e3ffd4b6a6e71f88577e7bd6204393
Bernard Banta <banta.bernard@gmail.com>
2013-02-27 15:34:32 +0300

    Added bootstrap will paginate for patients index

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -5,7 +5,7 @@ class PatientsController < ApplicationController
   # GET /patients.json
   def index
     #if current_user
-    @patients = Patient.all
+    @patients = Patient.paginate(:per_page => 10, :page => params[:page])
 
     respond_to do |format|
       format.html # index.html.erb
