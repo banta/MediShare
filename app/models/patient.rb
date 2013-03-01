ecd169359a0e018e0174e8af8c51fb86b17fe560
Bernard Banta <banta.bernard@gmail.com>
2013-03-01 16:12:40 +0300

    Added sphinx to patients

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -2,4 +2,13 @@ class Patient < ActiveRecord::Base
   belongs_to :health_facility
   attr_accessible :address, :civil_status, :country, :date_of_birth, :gender,
   :identification_number, :location, :names, :occupation, :phone, :town, :health_facility_id
+
+  define_index do
+    # fields
+    indexes address, civil_status, country, date_of_birth, gender,
+    identification_number, location, names, occupation, phone, town, health_facility_id
+    
+    # attributes
+    has id, created_at, updated_at
+  end
 end
