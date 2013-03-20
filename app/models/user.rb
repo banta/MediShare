7e62d2356a3707950421506d3d2611af9046c1b4
Bernard Banta <banta.bernard@gmail.com>
2013-03-20 10:45:03 +0300

    Added authentication token to the project

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,13 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :token_authenticatable
+
+  before_save :ensure_authentication_token
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :role_ids, :as => :admin
-  attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :role
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :role, :authentication_token
   attr_accessor :role
 
   private
