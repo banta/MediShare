b83df68306461fd7fc25bdf809e8961666cbe204
Bernard Banta <banta.bernard@gmail.com>
2013-04-09 15:01:23 +0300

    Added create and show method in patients api

--- app/controllers/api/private/v1/patients_controller.rb
+++ app/controllers/api/private/v1/patients_controller.rb
@@ -5,4 +5,31 @@ class Api::Private::V1::PatientsController < Api::Private::V1::BaseController
   	@patients = Patient.all
     respond_with(@patients, :api_template => :v1_patients_private, :status => :ok)
   end
+
+  def create
+    @patient = Patient.new(params[:patient])
+    @patient.names = params[:names]
+    @patient.identification_number = params[:identification_number]
+    @patient.gender = params[:gender]
+    @patient.address = params[:address]
+    @patient.town = params[:town]
+    @patient.location = params[:location]
+    @patient.phone = params[:phone]
+    @patient.country = params[:country]
+    @patient.occupation = params[:occupation]
+    @patient.civil_status = params[:civil_status]
+    @patient.date_of_birth = params[:date_of_birth]
+    @patient.health_facility_id = params[:health_facility_id]
+
+    if @patient.save
+      respond_with(@patient, :api_template => :v1_patients_private, :status => :ok)
+    else
+      respond_with(@patient.errors, :status => :unprocessable_entity)
+    end
+  end
+
+  def show
+    @applicant = Patient.find(params[:id])
+    respond_with(@applicant, :api_template => :v1_patients_private, :status => :ok)
+  end
 end
