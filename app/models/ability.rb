cc6e25e71fa5509020aba186b34f15dca83e0fae
Bernard Banta <banta.bernard@gmail.com>
2013-04-11 05:46:17 +0300

    Wrote users and admin abilities on the diseases.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,6 +7,7 @@ class Ability
       can :manage, :all
     elsif user.has_role? :user
         can [:show, :update, :destroy], HealthFacility, :user_id => user.id
+        can [:show, :update, :create, :index], Disease
         can :manage, Patient
     end
     # Define abilities for the passed in user here. For example:
