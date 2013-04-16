1f34ab230ff729cb5e75843a4a24b6901d607daa
Bernard Banta <banta.bernard@gmail.com>
2013-04-17 00:52:24 +0300

    Added prescriptions controller and views

--- app/controllers/prescriptions_controller.rb
+++ app/controllers/prescriptions_controller.rb
@@ -1,81 +1,81 @@
-class DiseasesController < ApplicationController
+class PrescriptionsController < ApplicationController
   before_filter :authenticate_user!
   load_and_authorize_resource
-  # GET /diseases
-  # GET /diseases.json
+  # GET /prescriptions
+  # GET /prescriptions.json
   def index
-    @diseases = Disease.all
+    @prescriptions = Prescription.all
 
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: @diseases }
+      format.json { render json: @prescriptions }
     end
   end
 
-  # GET /diseases/1
-  # GET /diseases/1.json
+  # GET /prescriptions/1
+  # GET /prescriptions/1.json
   def show
-    @disease = Disease.find(params[:id])
+    @prescription = Prescription.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
-      format.json { render json: @disease }
+      format.json { render json: @prescription }
     end
   end
 
-  # GET /diseases/new
-  # GET /diseases/new.json
+  # GET /prescriptions/new
+  # GET /prescriptions/new.json
   def new
-    @disease = Disease.new
+    @prescription = Prescription.new
 
     respond_to do |format|
       format.html # new.html.erb
-      format.json { render json: @disease }
+      format.json { render json: @prescription }
     end
   end
 
-  # GET /diseases/1/edit
+  # GET /prescriptions/1/edit
   def edit
-    @disease = Disease.find(params[:id])
+    @prescription = Prescription.find(params[:id])
   end
 
-  # POST /diseases
-  # POST /diseases.json
+  # POST /prescriptions
+  # POST /prescriptions.json
   def create
-    @disease = Disease.new(params[:disease])
+    @prescription = Prescription.new(params[:prescription])
 
     respond_to do |format|
-      if @disease.save
-        format.html { redirect_to @disease, notice: 'Disease was successfully created.' }
-        format.json { render json: @disease, status: :created, location: @disease }
+      if @prescription.save
+        format.html { redirect_to @prescription, notice: 'Prescription was successfully created.' }
+        format.json { render json: @prescription, status: :created, location: @prescription }
       else
         format.html { render action: "new" }
-        format.json { render json: @disease.errors, status: :unprocessable_entity }
+        format.json { render json: @prescription.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PUT /diseases/1
-  # PUT /diseases/1.json
+  # PUT /prescriptions/1
+  # PUT /prescriptions/1.json
   def update
-    @disease = Disease.find(params[:id])
+    @prescription = Prescription.find(params[:id])
 
     respond_to do |format|
-      if @disease.update_attributes(params[:disease])
-        format.html { redirect_to @disease, notice: 'Disease was successfully updated.' }
+      if @prescription.update_attributes(params[:prescription])
+        format.html { redirect_to @prescription, notice: 'Prescription was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
-        format.json { render json: @disease.errors, status: :unprocessable_entity }
+        format.json { render json: @prescription.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /diseases/1
-  # DELETE /diseases/1.json
+  # DELETE /prescriptions/1
+  # DELETE /prescriptions/1.json
   def destroy
-    @disease = Disease.find(params[:id])
-    @disease.destroy
+    @prescription = Prescription.find(params[:id])
+    @prescription.destroy
 
     respond_to do |format|
       format.html { redirect_to diseases_url }
