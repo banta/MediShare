bf68e67b91f2b9c1df7fa6a80eb27d55d0725bf8
Bernard Banta <banta.bernard@gmail.com>
2013-04-18 18:14:45 +0300

    Added associations and validations to prescription_disease model

--- app/models/prescription_disease.rb
+++ app/models/prescription_disease.rb
@@ -1,3 +1,11 @@
 class PrescriptionDisease < ActiveRecord::Base
+  #Accessible attributes
   attr_accessible :disease_id, :prescription_id
+
+  #Associations
+  belongs_to :prescription
+  belongs_to :disease
+
+  #Validations
+  validates :disease_id, :prescription_id, :presence => true
 end
