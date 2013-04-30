57b6dc5794cadbfee475f6bc4d526454910b6024
Bernard Banta <banta.bernard@gmail.com>
2013-04-30 14:27:38 +0300

    Created the assc btw data_mining and dmresults

--- app/models/data_mining.rb
+++ app/models/data_mining.rb
@@ -1,3 +1,6 @@
 class DataMining < ActiveRecord::Base
   # attr_accessible :title, :body
+
+  #Associations
+  has_many :dmresults
 end
--- app/models/dmresult.rb
+++ app/models/dmresult.rb
@@ -1,3 +1,8 @@
 class Dmresult < ActiveRecord::Base
+  
+  #Accessible attributes
   attr_accessible :confidence, :rule, :support, :total
+
+  #Associations
+  belongs_to :data_mining
 end
